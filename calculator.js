cfdb69bbfbbb90e1a6e0619f6d66544db6f68dc0
Jovan Sreco <jovan.sreco@gmail.com>
2022-08-10 13:01:25 +0200

    Version 1.0.1

--- calculator.js
+++ calculator.js
@@ -1,75 +1,76 @@
 export default class Calculator {
-    constructor(previousOperandElement, currentOperandElement) {
-        this.previousOperandElement = previousOperandElement
-        this.currentOperandElement = currentOperandElement
-        this.clearAll()
-    }
+  constructor(previousOperandElement, currentOperandElement) {
+    this.previousOperandElement = previousOperandElement;
+    this.currentOperandElement = currentOperandElement;
+    this.clearAll();
+  }
 
-    clearAll() {
-        this.previousOperand = ""
-        this.currentOperand = ""
-        this.operation = undefined
-    }
+  clearAll() {
+    this.previousOperand = "";
+    this.currentOperand = "";
+    this.operation = undefined;
+  }
 
-    delete() {
-        this.currentOperand = this.currentOperand.slice(0, -1)
-        this.updateDisplay()
-    }
+  delete() {
+    this.currentOperand = this.currentOperand.slice(0, -1);
+    this.updateDisplay();
+  }
 
-    appendNumber(number) {
-        if(this.currentOperand.includes(".") && number === ".") return
-        this.currentOperand += number.toString()
-    }
+  appendNumber(number) {
+    if (this.currentOperand.includes(".") && number === ".") return;
+    this.currentOperand += number.toString();
+  }
 
-    selectOperation(operation) {
-        if(!this.currentOperand && this.previousOperand) {
-            this.changeOperation(operation)
-            return
-        }
-        if(!this.currentOperand) return
-
-        if(this.previousOperand) {
-            this.calculate()
-        }
-        this.operation = operation
-        this.previousOperand = this.currentOperand
-        this.currentOperand = ""
+  selectOperation(operation) {
+    if (!this.currentOperand && this.previousOperand) {
+      this.changeOperation(operation);
+      return;
     }
+    if (!this.currentOperand) return;
 
-    calculate() {
-        let result
-        const number1 = parseFloat(this.previousOperand)
-        const number2 = parseFloat(this.currentOperand)
-        if(isNaN(number1) || isNaN(number2)) return
-        switch (this.operation) {
-            case "+":
-                result = number1 + number2
-                break
-            case "-":
-                result = number1 - number2
-                break
-            case "*":
-                result = number1 * number2
-                break
-            case "/":
-                result = number1 / number2
-                break
-            default:
-                return
-        }
-        this.currentOperand = result.toString()
-        this.operation = undefined
-        this.previousOperand = ''
+    if (this.previousOperand) {
+      this.calculate();
     }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-    changeOperation(operation) {
-        this.operation = operation
-        this.previousOperandElement.innerText = `${this.previousOperand} ${this.operation}`
+  calculate() {
+    let result;
+    const number1 = parseFloat(this.previousOperand);
+    const number2 = parseFloat(this.currentOperand);
+    if (isNaN(number1) || isNaN(number2)) return;
+    switch (this.operation) {
+      case "+":
+        result = number1 + number2;
+        break;
+      case "-":
+        result = number1 - number2;
+        break;
+      case "*":
+        result = number1 * number2;
+        break;
+      case "/":
+        result = number1 / number2;
+        break;
+      default:
+        return;
     }
+    this.currentOperand = result.toString();
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
 
-    updateDisplay() {
-        this.previousOperandElement.innerText = this.previousOperand
-        if(this.operation) this.previousOperandElement.innerText += " " + this.operation.toString()
-        this.currentOperandElement.innerText = this.currentOperand
-    }
+  changeOperation(operation) {
+    this.operation = operation;
+    this.previousOperandElement.innerText = `${this.previousOperand} ${this.operation}`;
+  }
+
+  updateDisplay() {
+    this.previousOperandElement.innerText = this.previousOperand;
+    if (this.operation)
+      this.previousOperandElement.innerText += " " + this.operation.toString();
+    this.currentOperandElement.innerText = this.currentOperand;
+  }
 }
--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-'use strict'
-import Calculator from "./calculator.js"
+"use strict";
+import Calculator from "./calculator.js";
 
-const numberBtns = document.querySelectorAll("[data-number]")
-const operationBtns = document.querySelectorAll("[data-operation]")
-const equalsBtn = document.querySelector("[data-equals]")
-const clearBtn = document.querySelector("[data-all-clear]")
-const delBtn = document.querySelector("[data-delete]")
-const previousOperandElement = document.querySelector("[data-previous-operand]")
-const currentOperandElement = document.querySelector("[data-current-operand]")
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const equalsBtn = document.querySelector("[data-equals]");
+const clearBtn = document.querySelector("[data-all-clear]");
+const delBtn = document.querySelector("[data-delete]");
+const previousOperandElement = document.querySelector("[data-previous-operand]");
+const currentOperandElement = document.querySelector("[data-current-operand]");
 
-const calculator = new Calculator(previousOperandElement, currentOperandElement)
+const calculator = new Calculator(
+  previousOperandElement,
+  currentOperandElement
+);
 
 // Number addition to display
-numberBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        calculator.appendNumber(btn.dataset.number)
-        calculator.updateDisplay()
-    })
-})
+numberBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    calculator.appendNumber(btn.dataset.number);
+    calculator.updateDisplay();
+  });
+});
 
-operationBtns.forEach(operation => {
-    operation.addEventListener("click", () => {
-        calculator.selectOperation(operation.dataset.operation)
-        calculator.updateDisplay()
-    })
-})
+operationBtns.forEach((operation) => {
+  operation.addEventListener("click", () => {
+    calculator.selectOperation(operation.dataset.operation);
+    calculator.updateDisplay();
+  });
+});
 
 equalsBtn.addEventListener("click", () => {
-    calculator.calculate()
-    calculator.updateDisplay()
-})
+  calculator.calculate();
+  calculator.updateDisplay();
+});
 
-clearBtn.addEventListener(("click"), () => {
-    calculator.clearAll()
-    calculator.updateDisplay()
-})
+clearBtn.addEventListener("click", () => {
+  calculator.clearAll();
+  calculator.updateDisplay();
+});
 
 delBtn.addEventListener("click", () => {
-    calculator.delete()
-    calculator.updateDisplay()
-})
+  calculator.delete();
+  calculator.updateDisplay();
+});
